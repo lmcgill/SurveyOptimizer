3f49de33413adbeeb748ed78b4b0caf972999215
Lillian McGill <lmcgill@uw.edu>
2022-10-25 16:35:35 -0700

    Fixed download button

--- SurveyOptimizer/app.R
+++ SurveyOptimizer/app.R
@@ -657,29 +657,28 @@ server <- function(input, output, session) {
   
   output$download_btn <- downloadHandler(
     #filename = function() {paste0(input$main, " vs ", input$control, " ", input$obRange[1], "-", input$obRange[2], ".xlsx")},
-    
+
     filename = function() {paste("SOM_results_", Sys.Date(), ".xlsx", sep = "")},
-    content = function(file) {
+    content = function(fname) {
       
-      fname <- paste(file,"xlsx",sep=".")
+     #fname <- paste(filename,"xlsx",sep=".")
       wb <- createWorkbook("Survey_shinyapp", "Survey results")
+      addWorksheet(wb, sheetName = "optimized_numbers")
+      addWorksheet(wb, sheetName = "enterprise_scores")
       addWorksheet(wb, sheetName = "objective_weights")
       addWorksheet(wb, sheetName = "bounds")
       addWorksheet(wb, sheetName = "totals")
-      addWorksheet(wb, sheetName = "data")
+      writeData(wb, data.frame(optimized.data()), sheet = "optimized_numbers")
+      writeData(wb, data.frame(enterprise.score()), sheet = "enterprise_scores")
       writeData(wb, data.frame(objective.weights.new$data), sheet = "objective_weights")
       writeData(wb,  data.frame(bounds.new$data), sheet = "bounds")
       writeData(wb,  data.frame(totals.new$data), sheet = "totals")
-      writeData(wb, enterprise.score(), sheet = "data")
-      saveWorkbook(wb, file = file)
+      saveWorkbook(wb, file = fname)
       
     },
     contentType = "file/xlsx"
   )
-  
-  output$TEMP <- renderText({
-    paste0("x=", "Dumb")
-  })
+
 }
 
 # Run the application 
